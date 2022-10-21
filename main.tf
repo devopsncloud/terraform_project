7ac58b2d5a0389ee44719a645a66452af19000e7
= <=>
2022-10-21 15:42:51 +0530

    terraform main file

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.35.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIASJCWOPWXVSLU3BOI"
+  secret_key = "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"
+}
